3fc42ae688557bbd61c7dcfdce832465eb2d0f93
ShanSanear <medan1993@gmail.com>
2020-12-25 23:41:10 +0100

    Added autorun.inf reader and burner to drive.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 from collections import namedtuple
 from pathlib import Path
@@ -31,6 +32,17 @@ def test_drive(current_letter):
     return is_drive_available
 
 
+def save_to_iso(drive, label, autorun_label, output_folder: Path):
+    file_name = Path(f"{label}_{autorun_label}").with_suffix(".iso")
+    params = [r"C:\Programs\ImgBurn\ImgBurn.exe", "/MODE", "READ", "/SRC", drive, "/DEST",
+              str(output_folder / file_name), "/EJECT", "/START", "/CLOSE", "/WAITFORMEDIA", "/LOG",
+              r"C:\temp\test.log", "/LOGHEADER"]
+    proc = subprocess.Popen(params, shell=True)
+    proc.wait()
+    status = proc.poll()
+    print("Status: {}".format(status))
+
+
 def list_files_in_drive(current_letter):
     return list(Path(current_letter).rglob("*.*"))
 
@@ -46,12 +58,13 @@ def main():
             drive_info[label] = files
             autorun_file = Path(f"{drive}Autorun.inf")
             if autorun_file.is_file():
-                    parser = ConfigParser()
-                    parser.read_string(autorun_file.read_text(encoding='utf-8'))
-                    autorun_label = parser['autorun']['label']
-                    print(f"autorun label: {autorun_label}")
-                    print(f"Standard label: {label}")
-                    print(f"List of files: {files}")
+                parser = ConfigParser()
+                parser.read_string(autorun_file.read_text(encoding='utf-8'))
+                autorun_label = parser['autorun']['label']
+                print(f"autorun label: {autorun_label}")
+                print(f"Standard label: {label}")
+                print(f"List of files: {files}")
+                save_to_iso(drive, label, autorun_label, output_folder=Path(r"C:\Users\Shan\Documents"))
 
         except pywintypes.error as err:
             print(err)
